88a0a6653abe0d35accd33fae6f3546b62b57c18
nulldev <me@nulldev.xyz>
2018-01-30 18:57:52 -0500

    Use green color in popup highlight when on safe page

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
 	if(request.action == "updateTabInfo") {
+		let color;
 		let status;
 		if(request.status) {
 			status = "DynamicHistory is hiding this page's history!";
+			color = "red";
 		} else {
 			status = "DynamicHistory is NOT hiding this page's history.";
+			color = "green";
 		}
 		document.getElementById("status").textContent = status;
 		document.getElementById("reason").textContent = request.reason;
@@ -13,7 +16,10 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 		if(targetText == null)
 			targetText = "";
 
-		document.getElementById("target").textContent = targetText;
+		let target = document.getElementById("target");
+
+		target.style.color = color;
+		target.textContent = targetText;
 	}
 });
 
